aca4e02ee0a5f937a39270dddcfd9b9ee9744c58
zhen jia <zjia0099@student.monash.edu>
2025-04-24 21:30:35 +1000

    changr

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import TimeDisplay from "@/component/TimeDisplay";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+    <div className="flex flex-col items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <main>
         <TimeDisplay />
       </main>
